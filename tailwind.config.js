b848ecc438c2c3a261caa29dad359ff0239170e9
Your Name <you@example.com>
2022-10-30 23:24:49 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "system-ui", "sans-serif"],
+        mono: ["Iosevka Aile", "Menlo", "Consolas", "monospace"],
+      },
+    },
+  },
+};

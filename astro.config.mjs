a1bbf0e007f06f71c1d80476e5064cf220b34993
Your Name <you@example.com>
2022-10-31 22:33:25 -0400

    style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
+import prefetch from "@astrojs/prefetch";
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-  integrations: [tailwind()],
+  integrations: [prefetch(), tailwind()],
   output: "server",
   site: "https://rolyreyes.com/",
 });

56a6a91b4819b6d82585523c8afffb53690fab14
Your Name <you@example.com>
2022-11-01 00:18:26 -0400

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", "system-ui", "sans-serif"],
-        mono: ["Iosevka Aile", "Menlo", "Consolas", "monospace"],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        mono: ["Iosevka Aile Web", ...defaultTheme.fontFamily.mono],
       },
     },
   },
